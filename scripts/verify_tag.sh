098082519cd00c5ad68c3565bf9bdfd6d2ef85f9
betolink <betolin@gmail.com>
2022-08-16 18:33:28 -0500

    fix tag verify

--- scripts/verify_tag.sh
+++ scripts/verify_tag.sh
@@ -3,11 +3,13 @@
 set -e
 
 git_tag=`git describe --tags --abbrev=0`
-current_version= current_version=v`cat pyproject.toml | grep version | cut -d'"' -f 2`
+current_version= current_version=v`cat pyproject.toml | grep -m 1 version | cut -d'"' -f 2`
 
-if [ $current_version = $git_tag ]; then
+echo "${git_tag} ${current_version}"
+
+if [ $current_version == $git_tag ]; then
   echo "Version does match git tag"
 else
-  echo "Version do not match git tag, Poetry ${current_version} vs Tag: ${git_tag} "
+  echo "Version does not match git tag, Poetry: ${current_version} vs Tag: ${git_tag} "
   exit 1
 fi
