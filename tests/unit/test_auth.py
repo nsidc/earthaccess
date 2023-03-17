64bcbad81b01945a312b7fb19377770598302feb
betolink <betolin@gmail.com>
2023-03-17 16:50:44 -0500

    add wrong credentials case to unit tests

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -2,6 +2,7 @@
 import unittest
 from unittest import mock
 
+import pytest
 import responses
 from earthaccess import Auth
 
@@ -83,3 +84,45 @@ class TestCreateAuth(unittest.TestCase):
         self.assertEqual(auth.authenticated, True)
         self.assertEqual(auth.password, "password")
         self.assertEqual(auth.token, json_response)
+
+    @responses.activate
+    @mock.patch("getpass.getpass")
+    @mock.patch("builtins.input")
+    def test_auth_fails_for_wrong_credentials(self, user_input, user_password) -> bool:
+        user_input.return_value = "bad_user"
+        user_password.return_value = "bad_password"
+        json_response = {"error": "wrong credentials"}
+
+        responses.add(
+            responses.GET,
+            "https://urs.earthdata.nasa.gov/api/users/tokens",
+            json=json_response,
+            status=401,
+        )
+        responses.add(
+            responses.GET,
+            "https://urs.earthdata.nasa.gov/profile",
+            json=json_response,
+            status=401,
+        )
+        responses.add(
+            responses.GET,
+            "https://urs.earthdata.nasa.gov/api/users/user?client_id=ntD0YGC_SM3Bjs-Tnxd7bg",
+            json=json_response,
+            status=401,
+        )
+        responses.add(
+            responses.POST,
+            "https://urs.earthdata.nasa.gov/api/users/token",
+            json=json_response,
+            status=401,
+        )
+        # Test
+        auth = Auth()
+        auth.login(strategy="interactive")
+        with pytest.raises(Exception) as e_info:
+            print(e_info)
+            self.assertEqual(auth.authenticated, False)
+            self.assertEqual(e_info, Exception)
+            self.assertEqual(auth.password, "password")
+            self.assertEqual(auth.token, json_response)
