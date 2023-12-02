07417c16111424f07db571144f3414ed12520479
Ian Carroll <ian.t.carroll@nasa.gov>
2023-12-02 02:54:35 +0000

    update for Amazon EC2 IMDSv2

--- earthaccess/store.py
+++ earthaccess/store.py
@@ -140,8 +140,10 @@ class Store(object):
         session = self.auth.get_session()
         try:
             # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
-            resp = session.get(
-                "http://169.254.169.254/latest/meta-data/public-ipv4", timeout=1
+            resp = session.put(
+                "http://169.254.169.254/latest/api/token",
+                headers={"X-aws-ec2-metadata-token-ttl-seconds": "21600"},
+                timeout=1,
             )
         except Exception:
             return False
