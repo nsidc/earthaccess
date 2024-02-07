6ec1081413ffd27ab5a2593ffe991608ddd3a289
danielfromearth <daniel.kaufman@nasa.gov>
2024-02-07 14:36:01 -0500

    typo

--- earthaccess/auth.py
+++ earthaccess/auth.py
@@ -228,13 +228,13 @@ class Auth(object):
         else:
             return {}
 
-    def _interactive(self, presist_credentials: bool = False) -> bool:
+    def _interactive(self, persist_credentials: bool = False) -> bool:
         username = input("Enter your Earthdata Login username: ")
         password = getpass.getpass(prompt="Enter your Earthdata password: ")
         authenticated = self._get_credentials(username, password)
         if authenticated:
             logger.debug("Using user provided credentials for EDL")
-            if presist_credentials:
+            if persist_credentials:
                 print("Persisting credentials to .netrc")
                 self._persist_user_credentials(username, password)
         return authenticated
