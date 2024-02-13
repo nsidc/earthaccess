9820912cb601f2d0eb19daabf9439c80b3aa2661
karthik venkataramani <karthikvt@vt.edu>
2024-02-12 22:11:21 -0800

    update to use pathlib

--- tests/integration/test_cloud_download.py
+++ tests/integration/test_cloud_download.py
@@ -1,6 +1,7 @@
 # package imports
 import logging
 import os
+from pathlib import Path
 import random
 import shutil
 import unittest
@@ -166,4 +167,4 @@ def test_multi_file_granule(tmp_path):
     urls = granules[0].data_links()
     assert len(urls) > 1
     files = earthaccess.download(granules, str(tmp_path))
-    assert set(map(os.path.basename, urls)) == set(map(os.path.basename, files))
+    assert set([Path(f).name for f in urls]) == set([Path(f).name for f in files])
