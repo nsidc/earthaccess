c6fecedcd6e9088f61708a8de03f69bfd0c22cae
betolink <betolin@gmail.com>
2024-02-29 06:36:11 -0600

    making ruff happy

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,11 +2,11 @@ import pytest
 
 ACCEPTABLE_FAILURE_RATE = 10
 
+
 @pytest.hookimpl()
 def pytest_sessionfinish(session, exitstatus):
-    if exitstatus==0:
+    if exitstatus == 0:
         return
     failure_rate = (100.0 * session.testsfailed) / session.testscollected
     if failure_rate <= ACCEPTABLE_FAILURE_RATE:
         session.exitstatus = 0
-
