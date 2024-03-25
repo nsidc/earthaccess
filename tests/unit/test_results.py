bc9c2f4e7e207434b40e95083d6e1f60e1ca2baf
Nikki <17799906+nikki-t@users.noreply.github.com>
2024-03-25 10:15:51 -0400

    Provide unit test for DataService get function

--- tests/unit/test_results.py
+++ tests/unit/test_results.py
@@ -1,4 +1,10 @@
+import json
+import unittest
+
 import earthaccess
+
+
+assertions = unittest.TestCase("__init__")
 
 
 def test_data_links():
@@ -19,3 +25,14 @@ def test_data_links():
     assert g.data_links(access="direct", in_region=False)[0].startswith("s3://")
     assert g.data_links(access="external", in_region=True)[0].startswith("https://")
     assert g.data_links(access="external", in_region=False)[0].startswith("https://")
+
+
+def test_services():
+    """Test DataService get function return of service metadata results."""
+    
+    query = earthaccess.services.DataService().parameters(concept_id="S2004184019-POCLOUD")
+    actual = query.get(query.hits())
+    actual = json.loads(actual[0])
+    with open("tests/unit/fixtures/S2004184019-POCLOUD.json") as jf:
+        expected = json.load(jf)
+    assertions.assertTrue(actual["items"] == expected[0]["items"])
