e14a98b5f7f7127ed657b86182f0d9573ec87317
danielfromearth <daniel.kaufman@nasa.gov>
2024-04-23 14:56:22 -0400

    mock CMR in UAT unit test and use earthaccess module to maintain environment

--- tests/unit/test_uat.py
+++ tests/unit/test_uat.py
@@ -1,11 +1,13 @@
 # package imports
+import json
 from unittest import mock
 
+import earthaccess
 import responses
-from earthaccess import Auth, search_data
 from earthaccess.auth import CLIENT_ID, Env
 
 
+# TODO: Still need to create an integration test, with corresponding credentials for UAT.
 class TestUatEnvironmentArgument:
     @responses.activate  # This will cause the test to check that all mocked URLs are hit.
     @mock.patch("getpass.getpass", new=mock.Mock(return_value="password"))
@@ -38,34 +40,30 @@ class TestUatEnvironmentArgument:
             status=200,
         )
 
-        # TODO: Add a mock for CMR query?  Does it need a "CMR-HITS" field in the response?
-        # responses.add(
-        #     responses.GET,
-        #     "https://cmr.uat.earthdata.nasa.gov/search/granules.umm_json?page_size=0",
-        #     json=json_response,
-        #     status=200,
-        # )
+        with open("fixtures/atl03_umm.json", "r") as f:
+            cmr_json_response = json.loads(f.read())
+
+        responses.add(
+            responses.GET,
+            "https://cmr.uat.earthdata.nasa.gov/search/granules.umm_json?page_size=0",
+            json=cmr_json_response,
+            headers={"CMR-Hits": "0"},
+            status=200,
+        )
 
         # Use Auth instance instead of the top-level (`earthaccess.`) API since this is a unit,
         # not an integration, test
-        auth = Auth()
-
+        auth = earthaccess.login()
         # Check that we're not already authenticated.
-        session = auth.get_session()
-        headers = session.headers
         assert not auth.authenticated
 
         # Login
-        auth.login(strategy="interactive", earthdata_environment=Env.UAT)
+        auth = earthaccess.login(strategy="interactive", earthdata_environment=Env.UAT)
         assert auth.authenticated
         assert auth.token in json_response
-        assert auth.earthdata_environment.value == Env.UAT.value
-
-        # Test that we are creating a session with the proper headers
-        assert "User-Agent" in headers
-        assert "earthaccess" in headers["User-Agent"]
+        assert auth.earthdata_environment["edl"] == Env.UAT["edl"]
 
         # Query CMR, and check that mock communication was with UAT CMR
-        results = search_data()
-        # Then possibly this:
-        assert len(results) == 0
+        results = earthaccess.search_data()
+        assert len(results) == 1
+        assert earthaccess.__auth__.earthdata_environment["edl"] == Env.UAT["edl"]
