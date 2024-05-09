525094f0e371ab547d57ce36465df4993694105f
danielfromearth <daniel.kaufman@nasa.gov>
2024-05-09 09:53:58 -0400

    remove unused SIT from system.py

--- earthaccess/system.py
+++ earthaccess/system.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 from typing_extensions import NewType
 
-from cmr import CMR_OPS, CMR_SIT, CMR_UAT
+from cmr import CMR_OPS, CMR_UAT
 
 CMRBaseURL = NewType("CMRBaseURL", str)
 EDLHostname = NewType("EDLHostname", str)
@@ -20,4 +20,3 @@ class System:
 
 PROD = System(CMRBaseURL(CMR_OPS), EDLHostname("urs.earthdata.nasa.gov"))
 UAT = System(CMRBaseURL(CMR_UAT), EDLHostname("uat.urs.earthdata.nasa.gov"))
-SIT = System(CMRBaseURL(CMR_SIT), EDLHostname("sit.urs.earthdata.nasa.gov"))
