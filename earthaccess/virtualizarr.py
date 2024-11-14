ed4a98b656ac7ae5392cde8700c8de2cf81aa22d
ayushnag <35325113+ayushnag@users.noreply.github.com>
2024-11-13 22:31:54 -0800

    add indirect access support and update docs

--- earthaccess/virtualizarr.py
+++ earthaccess/virtualizarr.py
@@ -5,74 +5,38 @@ from typing import TYPE_CHECKING
 import earthaccess
 
 if TYPE_CHECKING:
-    import fsspec
     import xarray as xr
 
-
-def _parse_dmr(
-    fs: fsspec.AbstractFileSystem,
-    data_path: str,
-    dmrpp_path: str = None,
-    group: str | None = None,
-) -> xr.Dataset:
-    """Parse a granule's DMR++ file and return a virtual xarray dataset.
-
-    Parameters
-    ----------
-    fs : fsspec.AbstractFileSystem
-        The file system to use to open the DMR++
-    data_path : str
-        The path to the data file
-    dmrpp_path : str
-        The path to the DMR++ file. If None, the DMR++ file is assumed to be at data_path + ".dmrpp"
-    group : str
-        The group to open in the DMR++.
-
-    Returns:
-    ----------
-    xr.Dataset
-        The virtual dataset (with virtualizarr ManifestArrays)
-
-    Raises:
-    ----------
-    Exception if the DMR++ file is not found or if there is an error parsing the DMR++
-    """
-    from earthaccess.dmrpp import DMRParser
-
-    dmrpp_path = data_path + ".dmrpp" if dmrpp_path is None else dmrpp_path
-    with fs.open(dmrpp_path) as f:
-        parser = DMRParser(f.read(), data_filepath=data_path)
-    return parser.parse_dataset(group=group)
-
-
 def open_virtual_mfdataset(
-    granules: list[earthaccess.results.DataGranule],
+    granules: list[earthaccess.DataGranule],
     group: str | None = None,
     access: str = "indirect",
     load: bool = False,
-    preprocess: callable | None = None,
+    preprocess: callable | None = None,  # type: ignore
     parallel: bool = True,
     **xr_combine_nested_kwargs,
 ) -> xr.Dataset:
     """Open multiple granules as a single virtual xarray Dataset.
 
+    Uses DMR++ metadata files to create a virtual xarray dataset with ManifestArrays. This virtual dataset can be used to create zarr reference files. See https://virtualizarr.readthedocs.io/en/latest/ for more information on virtual xarray datasets.
+
     Parameters
     ----------
-    granules : list[earthaccess.results.DataGranule]
+    granules : list[earthaccess.DataGranule]
         The granules to open
-    group : str
-        The group to open in the DMR++. If groups are present in the DMR++ files, this will open the specified group. If None, the first parsed group will be opened.
+    group : str or None (default=None)
+        The group to open in the DMR++. If groups are present in the DMR++ files, this will open the specified group. If None, the root group will be opened.
         If the DMR++ file does not have groups, this parameter is ignored.
-    access : str
-        The access method to use. One of "direct" or "indirect". Direct is for S3/cloud access, indirect is for HTTPS access.
-    load: bool
-        Create an xarray dataset with indexes and data available to load.
+    access : str (default="indirect")
+        The access method to use. One of "direct" or "indirect". Use direct when running on AWS, use indirect when running on a local machine.
+    load : bool (default=False)
+        Create an xarray dataset with indexes and lazy loaded data.
 
-        When true, creates a lazy loaded, numpy/dask backed xarray dataset with indexes. Note that when load=True all the data is now available to access but not loaded into memory. When false a virtual xarray dataset is created with ManifestArrays. This virtual dataset is a view over the underlying metadata and chunks and allows creation and concatenation of zarr reference files. This virtual dataset cannot load data on it's own and see https://virtualizarr.readthedocs.io/en/latest/ for more information on virtual xarray datasets. Also note that load=True will only work in the cloud since ranged reads of chunks are supported by cloud storage but not by HTTPS
-    preprocess : callable
+        When true, creates a lazy loaded, numpy/dask backed xarray dataset with indexes. Note that when `load=True` all the data is now available to access but not loaded into memory. When `load=False` a virtual xarray dataset is created with ManifestArrays. This virtual dataset is a view over the underlying metadata and chunks and allows creation and concatenation of zarr reference files. This virtual dataset cannot load data on it's own and see https://virtualizarr.readthedocs.io/en/latest/ for more information on virtual xarray datasets.
+    preprocess : callable (default=None)
         A function to apply to each virtual dataset before combining
-    parallel : bool
-        Whether to open the virtual datasets in parallel (using dask.delayed)
+    parallel : bool (default=True)
+       Open the virtual datasets in parallel (using dask.delayed)
     xr_combine_nested_kwargs : dict
         Keyword arguments for xarray.combine_nested.
         See https://docs.xarray.dev/en/stable/generated/xarray.combine_nested.html
@@ -81,12 +45,12 @@ def open_virtual_mfdataset(
     ----------
     xr.Dataset
 
-    Example:
+    Examplea:
     ----------
-    >>> results = earthaccess.search_data(count=5, short_name="MUR-JPL-L4-GLOB-v4.1")
+    >>> results = earthaccess.search_data(count=5, temporal=("2024"), short_name="MUR-JPL-L4-GLOB-v4.1")
     >>> vds = open_virtual_mfdataset(results, access="indirect", load=False, concat_dim="time", coords='minimal', compat='override', combine_attrs="drop_conflicts")
     >>> vds
-    <xarray.Dataset> Size: 19GB
+    <xarray.Dataset> Size: 29GB
     Dimensions:           (time: 5, lat: 17999, lon: 36000)
     Coordinates:
         time              (time) int32 20B ManifestArray<shape=(5,), dtype=int32,...
@@ -95,90 +59,115 @@ def open_virtual_mfdataset(
     Data variables:
         mask              (time, lat, lon) int8 3GB ManifestArray<shape=(5, 17999...
         sea_ice_fraction  (time, lat, lon) int8 3GB ManifestArray<shape=(5, 17999...
+        dt_1km_data       (time, lat, lon) int8 3GB ManifestArray<shape=(5, 17999...
         analysed_sst      (time, lat, lon) int16 6GB ManifestArray<shape=(5, 1799...
         analysis_error    (time, lat, lon) int16 6GB ManifestArray<shape=(5, 1799...
-    Attributes: (12/41)
-        Conventions:                CF-1.5
+        sst_anomaly       (time, lat, lon) int16 6GB ManifestArray<shape=(5, 1799...
+    Attributes: (12/42)
+        Conventions:                CF-1.7
+        title:                      Daily MUR SST, Final product
+
+    >>> vds = open_virtual_mfdataset(results, access="indirect", load=True, concat_dim="time", coords='minimal', compat='override', combine_attrs="drop_conflicts")
+    >>> vds
+    <xarray.Dataset> Size: 143GB
+    Dimensions:           (time: 5, lat: 17999, lon: 36000)
+    Coordinates:
+    * lat               (lat) float32 72kB -89.99 -89.98 -89.97 ... 89.98 89.99
+    * lon               (lon) float32 144kB -180.0 -180.0 -180.0 ... 180.0 180.0
+    * time              (time) datetime64[ns] 40B 2024-01-01T09:00:00 ... 2024-...
+    Data variables:
+        analysed_sst      (time, lat, lon) float64 26GB dask.array<chunksize=(1, 3600, 7200), meta=np.ndarray>
+        analysis_error    (time, lat, lon) float64 26GB dask.array<chunksize=(1, 3600, 7200), meta=np.ndarray>
+        dt_1km_data       (time, lat, lon) timedelta64[ns] 26GB dask.array<chunksize=(1, 4500, 9000), meta=np.ndarray>
+        mask              (time, lat, lon) float32 13GB dask.array<chunksize=(1, 4500, 9000), meta=np.ndarray>
+        sea_ice_fraction  (time, lat, lon) float64 26GB dask.array<chunksize=(1, 4500, 9000), meta=np.ndarray>
+        sst_anomaly       (time, lat, lon) float64 26GB dask.array<chunksize=(1, 3600, 7200), meta=np.ndarray>
+    Attributes: (12/42)
+        Conventions:                CF-1.7
         title:                      Daily MUR SST, Final product
     """
     import xarray as xr
 
-    if access == "indirect" and load:
-        raise ValueError("load=True is not supported with access='indirect'")
+    import virtualizarr as vz
 
     if access == "direct":
-        fs = earthaccess.get_s3_filesystem(results=granules)
-        fs.storage_options["anon"] = False
+        fs = earthaccess.get_s3_filesystem(results=granules[0])
+        fs.storage_options["anon"] = False  # type: ignore
     else:
         fs = earthaccess.get_fsspec_https_session()
     if parallel:
-        # wrap _open_virtual_dataset and preprocess with delayed
         import dask
 
-        open_ = dask.delayed(_parse_dmr)
+        # wrap _open_virtual_dataset and preprocess with delayed
+        open_ = dask.delayed(vz.open_virtual_dataset)  # type: ignore
         if preprocess is not None:
-            preprocess = dask.delayed(preprocess)
+            preprocess = dask.delayed(preprocess)  # type: ignore
     else:
-        open_ = _parse_dmr
-    # data_paths: list[str] = []
+        open_ = vz.open_virtual_dataset
     vdatasets = []
+    # Get list of virtual datasets (or dask delayed objects)
     for g in granules:
-        # data_paths.append(g.data_links(access=access)[0])
         vdatasets.append(
-            open_(fs=fs, group=group, data_path=g.data_links(access=access)[0])
+            open_(
+                filepath=g.data_links(access=access)[0] + ".dmrpp",
+                filetype="dmrpp",  # type: ignore
+                group=group,
+                indexes={},
+                reader_options={"storage_options": fs.storage_options},  # type: ignore
+            )
         )
-    # Rename paths to match granule s3/https paths
-    # vdatasets = [
-    # vds.virtualize.rename_paths(data_paths[i]) for i, vds in enumerate(vdatasets)
-    # ]
     if preprocess is not None:
         vdatasets = [preprocess(ds) for ds in vdatasets]
     if parallel:
-        vdatasets = dask.compute(vdatasets)[0]
+        vdatasets = dask.compute(vdatasets)[0]  # type: ignore
     if len(vdatasets) == 1:
         vds = vdatasets[0]
     else:
         vds = xr.combine_nested(vdatasets, **xr_combine_nested_kwargs)
     if load:
-        options = fs.storage_options.copy()
         refs = vds.virtualize.to_kerchunk(filepath=None, format="dict")
-        options["fo"] = refs
         return xr.open_dataset(
             "reference://",
             engine="zarr",
             chunks={},
-            backend_kwargs={"storage_options": options, "consolidated": False},
+            backend_kwargs={
+                "consolidated": False,
+                "storage_options": {
+                    "fo": refs,
+                    "remote_protocol": fs.protocol,
+                    "remote_options": fs.storage_options,  # type: ignore
+                },
+            },
         )
     return vds
 
 
 def open_virtual_dataset(
-    granule: earthaccess.results.DataGranule,
-    group: str | None = None,
+    granule: earthaccess.DataGranule,
     access: str = "indirect",
-    load: bool = True,
+    load: bool = False,
 ) -> xr.Dataset:
     """Open a granule as a single virtual xarray Dataset.
 
+    Uses DMR++ metadata files to create a virtual xarray dataset with ManifestArrays. This virtual dataset can be used to create zarr reference files. See https://virtualizarr.readthedocs.io/en/latest/ for more information on virtual xarray datasets.
+
     Parameters
     ----------
-    granule : earthaccess.results.DataGranule
+    granule : earthaccess.DataGranule
         The granule to open
-    group : str
-        The group to open in the DMR++. If groups are present in the DMR++ files, this will open the specified group. If None, the first parsed group will be opened.
-        If the DMR++ file does not have groups, this parameter is ignored.
-    access : str
-        The access method to use. One of "direct" or "indirect". Direct is for S3/cloud access, indirect is for HTTPS access.
-    load: bool
-        When true, creates a numpy/dask backed xarray dataset. When false a virtual xarray dataset is created with ManifestArrays
-        This virtual dataset cannot load data and is used to create zarr reference files. See https://virtualizarr.readthedocs.io/en/latest/
-        for more information on virtual xarray datasets
+    access : str (default="indirect")
+        The access method to use. One of "direct" or "indirect". Use direct when running on AWS, use indirect when running on a local machine.
+    load: bool (default=False)
+        Create an xarray dataset with indexes and lazy loaded data.
+
+        When true, creates a lazy loaded, numpy/dask backed xarray dataset with indexes. Note that when `load=True` all the data is now available to access but not loaded into memory. When `load=False` a virtual xarray dataset is created with ManifestArrays. This virtual dataset is a view over the underlying metadata and chunks and allows creation and concatenation of zarr reference files. This virtual dataset cannot load data on it's own and see https://virtualizarr.readthedocs.io/en/latest/ for more information on virtual xarray datasets.
+    
 
     Returns:
     ----------
     xr.Dataset
 
-    Example:
+    Examples:
     ----------
     >>> results = earthaccess.search_data(count=2, temporal=("2023"), short_name="SWOT_L2_LR_SSH_Expert_2.0")
     >>> open_virtual_dataset(results[0], access="indirect", load=False)
@@ -193,24 +182,9 @@ def open_virtual_dataset(
     Dimensions without coordinates: num_lines, num_pixels, num_sides
     Data variables: (12/98)
         height_cor_xover_qual                  (num_lines, num_pixels) uint8 681kB ManifestArray<shape=(9866, 69), dtype=uint8, chunks=(9866, 69...
-
-    >>> results = earthaccess.search_data(count=2, short_name="ATL03")
-    >>> open_virtual_dataset(results[0], group=""/gt1r/geolocation"" access="indirect", load=False)
-    <xarray.Dataset> Size: 27MB
-    Dimensions:                  (delta_time: 149696, ds_surf_type: 5, ds_xyz: 3)
-    Coordinates:
-        delta_time               (delta_time) float64 1MB ManifestArray<shape=(14...
-        reference_photon_lon     (delta_time) float64 1MB ManifestArray<shape=(14...
-        reference_photon_lat     (delta_time) float64 1MB ManifestArray<shape=(14...
-    Dimensions without coordinates: ds_surf_type, ds_xyz
-    Data variables: (12/34)
-        surf_type                (delta_time, ds_surf_type) int8 748kB ManifestAr...
-        podppd_flag              (delta_time) int8 150kB ManifestArray<shape=(149...
-        segment_ph_cnt           (delta_time) int32 599kB ManifestArray<shape=(14...
     """
     return open_virtual_mfdataset(
         granules=[granule],
-        group=group,
         access=access,
         load=load,
         parallel=False,
