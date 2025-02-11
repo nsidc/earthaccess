1c5de64040195a697cc6e79dcbf25d1080761bea
Matt Fisher <3608264+mfisher87@users.noreply.github.com>
2025-02-10 17:24:21 -0700

    More explicit differentiation between exception classes
    Co-authored-by: Joseph H Kennedy <me@jhkennedy.org>

--- earthaccess/exceptions.py
+++ earthaccess/exceptions.py
@@ -4,8 +4,9 @@ class LoginStrategyUnavailable(Exception):
     This should be raised when a login strategy can't be attempted, for example because
     "environment" was selected but the envvars are not populated.
 
-    DO NOT raise this exception when a login strategy is attempted and failed, for
-    example because credentials were rejected.
+    DO NOT raise this exception when a login strategy is attempted and fails. For
+    example, this exception would not be thrown when credentials were rejected;
+    a `LoginAttemptFailure` should be thrown instead.
     """
 
     pass
