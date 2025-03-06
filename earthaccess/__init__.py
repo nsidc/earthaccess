c73b136e4ba92f3c6024f775944603d3821c30a8
Dean Henze <deanchenze@gmail.com>
2025-03-06 10:11:23 -0800

    Add get_virtual_reference(), in testing

--- earthaccess/__init__.py
+++ earthaccess/__init__.py
@@ -21,8 +21,7 @@ from .api import (
     search_services,
 )
 from .auth import Auth
-from .dmrpp_zarr import open_virtual_dataset, open_virtual_mfdataset
-from .kerchunk import consolidate_metadata
+from .zarr import open_virtual_dataset, open_virtual_mfdataset, consolidate_metadata, get_virtual_reference
 from .search import DataCollection, DataCollections, DataGranule, DataGranules
 from .services import DataServices
 from .store import Store
@@ -59,6 +58,7 @@ __all__ = [
     "Store",
     # kerchunk
     "consolidate_metadata",
+    "get_virtual_reference",
     # virtualizarr
     "open_virtual_dataset",
     "open_virtual_mfdataset",
