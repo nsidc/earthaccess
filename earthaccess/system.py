b9b1a20b84f2bb1ee707ed0db0067c51a40a9721
Sherwin-14 <varghesesheriwn@gmail.com>
2025-07-03 20:28:38 +0530

    Added StatusURL and StatusApiURL PROD and UAT instances

--- earthaccess/system.py
+++ earthaccess/system.py
@@ -8,15 +8,25 @@ from cmr import CMR_OPS, CMR_UAT
 
 CMRBaseURL = NewType("CMRBaseURL", str)
 EDLHostname = NewType("EDLHostname", str)
+StatusURL = NewType("StatusURL", str)
+StatusApiURL = NewType("StatusApiURL", str)
 
+STATUS_PROD_URL = 'https://status.earthdata.nasa.gov/'
+STATUS_UAT_URL = 'https://status.uat.earthdata.nasa.gov/'
+STATUS_API_PROD_URL = 'https://status.earthdata.nasa.gov/api/v1/statuses'
+STATUS_API_UAT_URL = 'https://status.uat.earthdata.nasa.gov/api/v1/statuses'
 
 @dataclass(frozen=True)
 class System:
     """Host URL options, for different Earthdata domains."""
 
     cmr_base_url: CMRBaseURL
+    status_url: StatusURL
+    status_api_url: StatusApiURL
     edl_hostname: EDLHostname
 
 
-PROD = System(CMRBaseURL(CMR_OPS), EDLHostname("urs.earthdata.nasa.gov"))
-UAT = System(CMRBaseURL(CMR_UAT), EDLHostname("uat.urs.earthdata.nasa.gov"))
+PROD = System(CMRBaseURL(CMR_OPS), StatusURL(STATUS_PROD_URL), StatusApiURL(STATUS_API_PROD_URL), EDLHostname("urs.earthdata.nasa.gov"))
+UAT = System(CMRBaseURL(CMR_UAT), StatusURL(STATUS_UAT_URL), StatusApiURL(STATUS_API_UAT_URL), EDLHostname("uat.urs.earthdata.nasa.gov"))
+
+
