8408e5811d428666e032fcb051fe1e16daa0d140
Ian Carroll <ian.t.carroll@nasa.gov>
2025-09-10 08:59:57 -0400

    add serialization to test

--- tests/integration/test_xarray.py
+++ tests/integration/test_xarray.py
@@ -1,25 +1,32 @@
-import io
-
 import fsspec
 import h5netcdf
 import xarray
+from dask.distributed import Client
 from earthaccess.store import EarthAccessFile
 
 
 def test_open_dataset():
-    buffer = io.BytesIO()
-    with h5netcdf.File(buffer, "w") as f:
-        f.dimensions = {"x": 3}
-        f.create_variable("data", ("x",), dtype="i4")
-        f.variables["data"][:] = xarray.DataArray([1, 2, 3])
     fs = fsspec.filesystem("memory")
-    fs_path = "mydir/myfile.h5"
-    fs.pipe(fs_path, buffer.getvalue())
-    with fs.open(fs_path, mode="rb") as f:
-        earthaccess_file = EarthAccessFile(f, granule="foo")
-        assert xarray.backends.list_engines()["h5netcdf"].guess_can_open(
-            earthaccess_file
-        )
-        file = xarray.open_dataset(earthaccess_file, engine="h5netcdf")
-        assert xarray.DataArray.all(file["data"].values == xarray.DataArray([1, 2, 3]))
+    values = list(range(32))
+    fs_path = "foo"
+    with fs.open(fs_path, mode="wb") as path:
+        with h5netcdf.File(path, "w") as f:
+            f.dimensions = {"x": len(values)}
+            f.create_variable("data", ("x",), dtype="i4")
+            f.variables["data"][:] = values
+    f = fs.open(fs_path)
+    earthaccess_file = EarthAccessFile(f, granule="foo")
+    # confirm backend detection
+    backends = xarray.backends.list_engines()
+    assert backends["h5netcdf"].guess_can_open(earthaccess_file)
+    # confirm opens
+    ds = xarray.open_dataset(earthaccess_file)
+    assert (ds["data"] == values).all()
+    # confirm serializes
+    client = Client()
+    future = client.submit(xarray.open_dataset, earthaccess_file)
+    ds = future.result()
+    assert isinstance(ds, xarray.Dataset)
+    # cleanup
+    client.shutdown()
     fs.store.clear()
