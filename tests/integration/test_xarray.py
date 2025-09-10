0111c8f92afca07915a666fcb390bd0fc9381ec1
Ian Carroll <ian.t.carroll@nasa.gov>
2025-09-10 14:14:57 -0400

    split tests, avoid h5netcdf import

--- tests/integration/test_xarray.py
+++ tests/integration/test_xarray.py
@@ -1,32 +1,20 @@
 import fsspec
-import h5netcdf
-import xarray
-from dask.distributed import Client
+import xarray as xr
 from earthaccess.store import EarthAccessFile
 
 
 def test_open_dataset():
+    ds = xr.Dataset({"y": ("x", list(range(8)))})
     fs = fsspec.filesystem("memory")
-    values = list(range(32))
-    fs_path = "foo"
-    with fs.open(fs_path, mode="wb") as path:
-        with h5netcdf.File(path, "w") as f:
-            f.dimensions = {"x": len(values)}
-            f.create_variable("data", ("x",), dtype="i4")
-            f.variables["data"][:] = values
-    f = fs.open(fs_path)
+    path = "foo"
+    with fs.open(path, mode="wb") as f:
+        ds.to_netcdf(f, engine="h5netcdf")
+    f = fs.open(path)
     earthaccess_file = EarthAccessFile(f, granule="foo")
     # confirm backend detection
-    backends = xarray.backends.list_engines()
+    backends = xr.backends.list_engines()
     assert backends["h5netcdf"].guess_can_open(earthaccess_file)
-    # confirm opens
-    ds = xarray.open_dataset(earthaccess_file)
-    assert (ds["data"] == values).all()
-    # confirm serializes
-    client = Client()
-    future = client.submit(xarray.open_dataset, earthaccess_file)
-    ds = future.result()
-    assert isinstance(ds, xarray.Dataset)
+    # confirm opens correctly
+    assert (xr.open_dataset(earthaccess_file) == ds).all()
     # cleanup
-    client.shutdown()
     fs.store.clear()
