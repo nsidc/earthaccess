03f638a591c42552ba42b86d091bc71013aa4abd
Tyler Sutterley <tsutterl@uw.edu>
2025-09-22 16:06:14 -0700

    Apply suggestions from code review
    Co-authored-by: Chuck Daniels <cjdaniels4@gmail.com>

--- earthaccess/search.py
+++ earthaccess/search.py
@@ -625,7 +625,7 @@ class DataGranules(GranuleQuery):
                 self.params["provider"] = provider
         return self
 
-    def granule_name(self, granule_name: str | list[str]) -> Self:
+    def granule_name(self, granule_name: str | Iterable[str]) -> Self:
         """Find granules matching either granule ur or producer granule id,
         queries using the readable_granule_name metadata field.
 
@@ -640,10 +640,15 @@ class DataGranules(GranuleQuery):
             self
 
         Raises:
-            TypeError: if `granule_name` is not of type `str` or `list[str]`.
+            TypeError: if `granule_name` is not of type `str` or `Iterable[str]`.
         """
-        if not isinstance(granule_name, (str, list)):
-            raise TypeError("granule_name must be of type string or list of strings")
+        if not isinstance(granule_name, Iterable):
+            raise TypeError("granule_name must be of type string or Iterable of strings")
+        if not isinstance(granule_name, str):
+            # Convert iterable to list of strings. Since str is also Iterable, make
+            # sure we don't do this when granule_name is a string, otherwise
+            # we would get a list of individual characters.
+            granule_name = list(str(name) for name in granule_name)
 
         self.params["readable_granule_name"] = granule_name
         self.params["options[readable_granule_name][pattern]"] = True
