6c1dad02932535b07f4788e9fee3991158df718f
s-kganz <ganzkeenan1@gmail.com>
2026-01-13 15:58:38 -0800

    add geo interface and tests

---
+++ tests/integration/test_geo_interface.py
@@ -0,0 +1,37 @@
+import geopandas as gpd
+
+COUNT = 10
+
+def _get_granules(collection: str, count: int=COUNT):
+    import earthaccess
+    earthaccess.login()
+    
+    return earthaccess.search_data(short_name=collection, count=count)
+
+def _check_geoms(geoms: gpd.GeoSeries):
+    assert(len(geoms) == 10)
+    assert(geoms.is_valid.all())
+
+def test_geo_gpolygons():
+    granules = _get_granules("MYD11A1")
+    geoms = gpd.GeoSeries(granules, crs=4326)
+    _check_geoms(geoms)
+    assert((geoms.geom_type == "MultiPolygon").all())
+
+def test_geo_bounding_rects():
+    granules = _get_granules("SRTMGL1")
+    geoms = gpd.GeoSeries(granules, crs=4326)
+    _check_geoms(geoms)
+    assert((geoms.geom_type == "MultiPolygon").all())
+    
+def test_geo_points():
+    granules = _get_granules("SNEX23_MAR23_SP")
+    geoms = gpd.GeoSeries(granules, crs=4326)
+    _check_geoms(geoms)
+    assert((geoms.geom_type == "MultiPoint").all())
+    
+def test_geo_linestrings():
+    granules = _get_granules("JASON_3_L2_OST_OGDR_GPS")
+    geoms = gpd.GeoSeries(granules, crs=4326)
+    _check_geoms(geoms)
+    assert((geoms.geom_type == "MultiLineString").all())
