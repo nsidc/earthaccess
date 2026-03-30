9a09ae59e3dfc7f32bf074055e07eb80ea806640
betolink <betolin@gmail.com>
2026-03-30 16:19:28 -0500

    fix mypy

--- earthaccess/virtual/core.py
+++ earthaccess/virtual/core.py
@@ -32,7 +32,7 @@ logger = logging.getLogger(__name__)
 
 
 def virtualize(
-    granules: Sequence[earthaccess.DataGranule],
+    granules: list[earthaccess.DataGranule],
     *,
     access: AccessType = "direct",
     load: bool = False,
