6b3f8cc333a9b032476be468e8e63dcded9cd8aa
Matt Fisher <mfisher87@gmail.com>
2026-04-14 17:34:08 -0600

    Add query object to Results class
    Co-authored-by: Trey Stafford <19692879+trey-stafford@users.noreply.github.com>
    Co-authored-by: Julia Lober <72712672+julober@users.noreply.github.com>
    Co-authored-by: Joseph H Kennedy <7882693+jhkennedy@users.noreply.github.com>

--- earthaccess/search.py
+++ earthaccess/search.py
@@ -5,14 +5,12 @@ from inspect import getmembers, ismethod
 from typing import (
     Any,
     Self,
+    SupportsFloat,
     TypeAlias,
+    override,
 )
 
 import requests
-from typing_extensions import (
-    SupportsFloat,
-    override,
-)
 
 from cmr import CollectionQuery, GranuleQuery
 
@@ -107,12 +105,13 @@ class DataCollections(CollectionQuery):
             [
                 DataCollection(collection, self._fields)
                 for collection in get_results(self.session, self, limit)
-            ]
+            ],
+            query=self,
         )
 
     @override
     def get_all(self) -> Results[DataCollection]:
-        return Results(super().get_all())
+        return Results(super().get_all(), query=self)
 
     @override
     def concept_id(self, IDs: Sequence[str]) -> Self:
@@ -494,11 +493,12 @@ class DataGranules(GranuleQuery):
 
         return Results(
             [DataGranule(granule, cloud_hosted=cloud) for granule in response],
+            query=self,
         )
 
     @override
     def get_all(self) -> Results[DataGranule]:
-        return Results(super().get_all())
+        return Results(super().get_all(), query=self)
 
     @override
     def parameters(self, **kwargs: Any) -> Self:
